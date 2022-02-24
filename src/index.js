4b7d8e904f06e488de621155f0f4a8610a813155
Julia Khvatova <jkhvatova@gmail.com>
2022-02-24 17:55:32 +0300

    feat: solved this brackets task, it was really hard and it took so long to find need of ObjectFromEntries

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const brackets = Object.fromEntries(bracketsConfig);
+  const openBrackets = Object.keys(brackets);
+  const swappedBracketsPairs = Object.fromEntries(Object.entries(brackets).map(
+    ([key, value]) => [value, key]
+  ));
+
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+
+
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+      let currentSymbol = str[i];
+      let topElement = stack[stack.length - 1];
+
+      if (openBrackets.includes(currentSymbol)) {
+
+        if (topElement === '7' && currentSymbol === '7' || topElement === '8' && currentSymbol === '8' || topElement === '|' && currentSymbol === '|') {
+          stack.pop();
+        } else {
+          stack.push(currentSymbol);
+        }
+
+      } else {
+        if (stack.length === 0) {
+          return false;
+        }
+   
+        if (swappedBracketsPairs[currentSymbol] === topElement) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+
+    return stack.length === 0;
 }
